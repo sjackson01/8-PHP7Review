ff01453205b44b2968580fc2e6ad077dbdf1be35
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-29 14:54:49 -0600

    Add (index.php>Hunk 1) : Example of try catch block with Throwable interface. Add (Interfaces.php>Hunk 1) : Example of PHP built in Throwable interface.

--- index.php
+++ index.php
@@ -16,3 +16,12 @@ function getTotal(float $a, float $b) : float {
 }
 
 getTotal('Wall', 1);
+
+//PHP 7 try catch block vs PHP 5
+try{
+    //Code that may throw an Exception or Error.
+}catch(Throwable $t){
+    //Execute only in PHP 7, will not match in PHP 5
+}catch(Exception $e){
+    //Execute only in PHP 5, will not be reaced in PHP 7
+}
